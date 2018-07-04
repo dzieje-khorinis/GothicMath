9d7eaada4b2ee0c3fd7599b25e9660f3096892aa
Artur <arturmiks@gmail.com>
2018-07-04 23:42:07 +0200

    Added correct opentk version

--- GothicMath/GothicMath.cs
+++ GothicMath/GothicMath.cs
@@ -3,6 +3,17 @@ using OpenTK;
 
 namespace GothicMathLib
 {
+    public class Size
+    {
+        public int Width;
+        public int Height;
+
+        public Size(int Width, int Height)
+        {
+            this.Width = Width;
+            this.Height = Height;
+        }
+    }
     public static class GothicMath
     {
         // Расчёт луча
